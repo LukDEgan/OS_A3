824d7565d73a2de54d1a051c36473e0f1df43892
Luke Egan <lukeegan2003@gmail.com>
2024-10-24 14:36:06 +1030

    changed includes

--- assignment3.c
+++ assignment3.c
@@ -1,9 +1,10 @@
 
-#include <libc.h>
+
 #include <netinet/in.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
